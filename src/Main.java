20e2f5022e307b7f19afd6742669c813b96655f9
Ercan Civi <ercancivi@gmail.com>
2021-01-26 01:03:16 -0600

    project is already in remote repo, committing more changes

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ public class Main {
         //B21 is good at IntelliJ
         //more stuff
         //Mac user and Windows users are happy to see commit dialog finally
+
+        //I have already published on Github.com
+        //now I am ready to make more commits
+        //but do I need to share again ????
+        //ABSOLUTELY NO
+        //IT'S TIME COMMIT AND JUST PUSH!!!!!
     }
 
 }
