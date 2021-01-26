778e126df8308596f2b7c6888e19af31d6d9c3cd
Ercan Civi <ercancivi@gmail.com>
2021-01-26 00:52:29 -0600

    added more comments into main method and fixed mac and windows commit dialog issue

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         //B21 is good at IntelliJ
+        //more stuff
+        //Mac user and Windows users are happy to see commit dialog finally
     }
 
 }
